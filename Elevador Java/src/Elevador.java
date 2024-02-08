781d8e7a8f41002d6b2d90f2c9d93f5a4a2e2117
mattmarquess <141771115+mattmarquess@users.noreply.github.com>
2024-02-07 21:54:51 -0300

    Add files via upload

---
+++ Elevador Java/src/Elevador.java
@@ -0,0 +1,16 @@
+public class Elevador {
+    //atributos
+    private int andarAtual;
+    //métodos
+    //construtor
+    public Elevador() {
+        this.andarAtual=0;
+    }
+    //getters and setters
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+    public void setAndaAtual(int andarAtual) {
+        this.andarAtual = andarAtual;
+    }
+}
